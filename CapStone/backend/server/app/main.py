e049a42e5e9376520a477f61b1529d9b125f29a7
donggyun kang <donggyun0905@daum.net>
2025-10-12 15:21:21 +0900

    Update main.py

--- CapStone/backend/server/app/main.py
+++ CapStone/backend/server/app/main.py
@@ -3,7 +3,7 @@
 import os
 import uvicorn
 from fastapi import FastAPI
-from .routers import ingest, view, data  # ← view.py도 이제 포함하자
+from .routers import ingest, view, data, fcm
 from fastapi.middleware.cors import CORSMiddleware
 from .schemas.database import engine
 from .models import health
@@ -23,6 +23,7 @@ app.add_middleware(
 app.include_router(ingest.router)
 app.include_router(view.router)
 app.include_router(data.router)
+app.include_router(fcm.router)
 
 # DB 테이블 생성 (초기 1회)
 health.Base.metadata.create_all(bind=engine)
@@ -36,3 +37,4 @@ def root():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
     uvicorn.run("app.main:app", host="0.0.0.0", port=port, reload=True)
+
